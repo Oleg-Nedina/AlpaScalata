1671cd1484cb2add1e6484bb691612b0c3107c4d
31/07/2027 <u10905938@gpu01.mate.polimi.it>
2025-12-23 20:41:42 +0100

    small data

---
+++ build/bench/micro/CMakeFiles/benchmark_alpaka.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for benchmark_alpaka.
+# This may be replaced when dependencies are built.
---
+++ build/src/CMakeFiles/gemm_backend_cuda.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/u10905938/AlpaScalata/src/backends/cuda/gemm_cuda.cu" "src/CMakeFiles/gemm_backend_cuda.dir/backends/cuda/gemm_cuda.cu.o" "gcc" "src/CMakeFiles/gemm_backend_cuda.dir/backends/cuda/gemm_cuda.cu.o.d"
+  "/home/u10905938/AlpaScalata/src/backends/cuda/gemm_cuda_naive.cu" "src/CMakeFiles/gemm_backend_cuda.dir/backends/cuda/gemm_cuda_naive.cu.o" "gcc" "src/CMakeFiles/gemm_backend_cuda.dir/backends/cuda/gemm_cuda_naive.cu.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
